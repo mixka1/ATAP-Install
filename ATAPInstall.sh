002342ad4a8d30f33736543ad47a1ae59ac5252e
Kaiden Mix <82608730+mixka1@users.noreply.github.com>
2024-10-14 19:17:30 -0400

    Update ATAPInstall.sh

--- ATAPInstall.sh
+++ ATAPInstall.sh
@@ -1,26 +1,47 @@
 #!/bin/bash
 
+#Set Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+CYAN='\033[0;36m'
+WHITE='\033[1;37m'
+RESET='\033[0m'
+
 # Check if the script is run as root
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
+   echo -e "${RED}This script must be run as root${RESET}" 
    exit 1
 fi
+# Define the required directory path
+DIR="/root/Documents"
 
+# Check if the directory exists
+if [ ! -d "$DIR" ]; then
+    echo "${YELLOW}Documents folder does not exist. Creating it...${RESET}"
+    sudo mkdir -p "$DIR"
+else
+    echo -e "${GREEN}Documents folder already exists.${RESET}"
+fi
 # Download the PowerShell .deb package
-echo "Downloading PowerShell..."
+echo -e "${YELLOW}Downloading PowerShell...${RESET}"
 wget https://github.com/PowerShell/PowerShell/releases/download/v7.4.5/powershell_7.4.5-1.deb_amd64.deb
 
 # Install the downloaded package
-echo "Installing PowerShell..."
+echo -e "${YELLOW}Installing PowerShell...${RESET}"
 dpkg -i powershell_7.4.5-1.deb_amd64.deb
 
 # Start PowerShell and install ATAPAuditor module
-echo "Starting PowerShell to install the ATAPAuditor module..."
-pwsh -Command "Install-Module -Name ATAPAuditor -Force; Save-ATAPHtmlReport -ReportName 'Ubuntu 22.04'"
+echo -e "${YELLOW}Starting PowerShell to install the ATAPAuditor module...${RESET}"
+sudo pwsh -Command "Install-Module -Name ATAPAuditor -Force; exit" 
+echo -e "${GREEN}PowerShell and ATAPAuditor installation complete!${RESET}"
+echo -e "${YELLOW}Running ATAP for Ubuntu 22.04${RESET}"
+sudo pwsh -Command "Save-ATAPHtmlReport -ReportName 'Ubuntu 22.04'"
 
 # Clean up by removing the downloaded .deb file
-echo "Cleaning up..."
+echo -e "${YELLOW}Cleaning up...${RESET}"
 rm powershell_7.4.5-1.deb_amd64.deb
 
-echo "PowerShell and ATAPAuditor installation complete!"
+
 
